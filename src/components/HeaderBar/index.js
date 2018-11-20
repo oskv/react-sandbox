b8239b7a5fbba2e5de3f0bf7704768d9842b869b
Alexandr <alex.skakosky@gmail.com>
2018-11-20 22:42:44 +0200

    add save button

--- src/components/HeaderBar/index.js
+++ src/components/HeaderBar/index.js
@@ -3,18 +3,55 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Icon from '@material-ui/core/Icon';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Button from '@material-ui/core/Button';
+import CheckIcon from '@material-ui/icons/Check';
+import SaveIcon from '@material-ui/icons/Save';
 
 export default class HeaderBar extends PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.handleSaveClick = this.handleSaveClick.bind(this);
+  }
+
+  state = {
+    processing: false,
+    saved: false,
+  };
+
   render() {
+    const { loading, saved } = this.state;
+    const buttonClass = saved ? 'saved' : '';
     return (
       <AppBar className="main-app-bar">
-        <Toolbar>
+        <Toolbar className='main-app-bar-toolbar'>
           <Icon className="logo-icon">email</Icon>
           <Typography variant="headline" color="inherit">
             Email editor
           </Typography>
+          <div className={`save-buttons-wrapper ${buttonClass}`}>
+              <Button
+                variant="fab"
+                mini
+                color="primary"
+                className='button-save'
+                onClick={this.handleSaveClick}
+              >
+                { saved ? <CheckIcon className='' /> : <SaveIcon />}
+              </Button>
+              {loading && <CircularProgress size={46} className='loader' />}
+          </div>
         </Toolbar>
       </AppBar>
     );
+  }
+
+  handleSaveClick() {
+    this.setState({ loading: true, saved: false });
+
+    setTimeout( () => {
+      this.setState({ loading: false, saved: true });
+    }, 2000)
   }
 }
